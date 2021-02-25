35c6e8fc159e26b119d2ce6c214f0ed2af48b041
Pacman99 <pachum99@myrdd.info>
2021-02-25 12:30:20 -0800

    nixos/matrix-appservices: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -863,6 +863,7 @@
   ./services/misc/llama-cpp.nix
   ./services/misc/logkeys.nix
   ./services/misc/mame.nix
+  ./services/misc/matrix-appservices/default.nix
   ./services/misc/mbpfan.nix
   ./services/misc/mediatomb.nix
   ./services/misc/metabase.nix
