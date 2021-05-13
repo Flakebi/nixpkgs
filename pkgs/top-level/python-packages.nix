2fad94072b1eee6c71514dcd1b13be0ae858ea99
Flakebi <flakebi@t-online.de>
2021-05-13 16:41:36 +0200

    python3Packages.tika: init at 1.24

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8653,6 +8653,8 @@ in {
 
   tifffile = callPackage ../development/python-modules/tifffile { };
 
+  tika = callPackage ../development/python-modules/tika { };
+
   tiledb = callPackage ../development/python-modules/tiledb {
     inherit (pkgs) tiledb;
   };
