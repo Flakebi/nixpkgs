0b059c5be691c6984f5c05022ebbf9b1bf6e5c3e
Flakebi <flakebi@t-online.de>
2021-07-05 23:12:22 +0200

    eww: Try to update

--- pkgs/applications/misc/eww/default.nix
+++ pkgs/applications/misc/eww/default.nix
@@ -12,16 +12,20 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "eww";
-  version = "unstable-2021-05-03";
+  version = "unstable-2021-06-23";
 
-  # TODO Needs a nightly compiler
+  # TODO Needs a nightly compiler or a patch to add features explicitely
+  # a nightly compiler is required unless we use this cheat code.
+  RUSTC_BOOTSTRAP=1;
 
   src = fetchFromGitHub {
     owner = "elkowar";
     repo = pname;
-    rev = "9ea20cd7537bd9637c42e36ba260e360f5d51440";
-    sha256 = "LPA2wu5ACv2n+y0JLD7o8q3JTtCzjcEGzLNmL8DZBkI=";
+    rev = "10d3d9375fa4e4e522aee9a21585c24e4c0009f5";
+    sha256 = "tLcwS8E1Qx7C9GLQcICgfxo3ou1wYmn6d4neQlg3+j0=";
   };
+
+  cargoSha256 = "XuuM5qf6JDDJ5pYlUvfv9Xn0Se+ynNZScJgAJWBboa0=";
 
   cargoFlags = lib.optionals useWayland [ "--no-default-features" "--features=wayland" ];
 
@@ -32,6 +36,7 @@ rustPlatform.buildRustPackage rec {
   buildInputs = [
     glib
     gtk3
+  ] ++ lib.optionals (!useWayland) [
     xorg.libX11
     xorg.libXcursor
     xorg.libXrandr
@@ -39,8 +44,6 @@ rustPlatform.buildRustPackage rec {
     xorg.libxcb
     xorg.libXrender
   ] ++ lib.optionals useWayland [ gtk-layer-shell wayland ];
-
-  cargoSha256 = "9dtW8tg93ju9Bx+PI9IahUHOSXpnajTv8BUgio6BoHs=";
 
   meta = with lib; {
     description = "A standalone widget system made in Rust that allows you to implement your own, custom widgets in any window manager";
