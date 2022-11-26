9130d4c0d7215b903b38cefc8c2680f109785362
Flakebi <flakebi@t-online.de>
2022-11-26 23:40:44 +0100

    fail2ban: reload after firewall reload
    Fix fail2ban not restarting when the firewall is reloaded.
    
    Previously, after the firewall is reloaded, fail2ban would be left in a
    broken state because all its firewall rules got deleted without fail2ban
    knowing.

--- nixos/modules/services/security/fail2ban.nix
+++ nixos/modules/services/security/fail2ban.nix
@@ -426,6 +426,13 @@ in
       dbfile = lib.mkDefault "/var/lib/fail2ban/fail2ban.sqlite3";
     };
 
+    # Restart after firewall rules are cleared.
+    # Start in parallel to avoid the circular dependency because fail2ban waits on the firewall.
+    systemd.services.firewall.serviceConfig.ExecReload = lib.optional config.networking.firewall.enable
+      "-/bin/sh -c '/run/current-system/systemd/bin/systemctl try-restart fail2ban.service &'";
+    systemd.services.nftables.serviceConfig.ExecReload = lib.optional config.networking.nftables.enable
+      "-/bin/sh -c '/run/current-system/systemd/bin/systemctl try-restart fail2ban.service &'";
+
     # Add some reasonable default jails.  The special "DEFAULT" jail
     # sets default values for all other jails.
     services.fail2ban.jails = lib.mkMerge [
