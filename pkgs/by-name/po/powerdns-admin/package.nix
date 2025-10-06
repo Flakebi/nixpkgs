ab265486c0b6dcf04fe18d669680fb0e35a17936
Flakebi <flakebi@t-online.de>
2025-10-06 20:52:55 +0200

    powerdns-admin: Fix db migration

--- pkgs/by-name/po/powerdns-admin/package.nix
+++ pkgs/by-name/po/powerdns-admin/package.nix
@@ -137,6 +137,10 @@ stdenv.mkDerivation {
 
   postPatch = ''
     rm -r powerdnsadmin/static powerdnsadmin/assets.py
+    substituteInPlace powerdnsadmin/__init__.py \
+      --replace-fail 'models.init_app(app)' ""
+    substituteInPlace powerdnsadmin/__init__.py \
+      --replace-fail 'models.base.db' 'models.base.db; models.init_app(app)'
   '';
 
   installPhase = ''
