b7310b837c3386ff279485b94650f13ecaa6c325
Flakebi <flakebi@t-online.de>
2026-05-14 23:48:34 +0200

    Reproduce works

--- nixos/tests/maddy/tls.nix
+++ nixos/tests/maddy/tls.nix
@@ -20,11 +20,6 @@ import ../make-test-python.nix (
             primaryDomain = domain;
             openFirewall = true;
             ensureAccounts = [ "postmaster@${domain}" ];
-            ensureCredentials = {
-              # Do not use this in production. This will make passwords world-readable
-              # in the Nix store
-              "postmaster@${domain}".passwordFile = "${pkgs.writeText "postmaster" "test"}";
-            };
             tls = {
               loader = "file";
               certificates = [
@@ -34,19 +29,67 @@ import ../make-test-python.nix (
                 }
               ];
             };
-            # Enable TLS listeners. Configuring this via the module is not yet
-            # implemented.
             config =
-              builtins.replaceStrings
-                [
-                  "imap tcp://0.0.0.0:143"
-                  "submission tcp://0.0.0.0:587"
-                ]
-                [
-                  "imap tls://0.0.0.0:993 tcp://0.0.0.0:143"
-                  "submission tls://0.0.0.0:465 tcp://0.0.0.0:587"
-                ]
-                options.services.maddy.config.default;
+              ''
+                debug true
+
+                auth.plain_separate send_authdb {
+                  pass &recv_authdb
+                }
+
+                storage.imapsql local_mailboxes {
+                  driver sqlite3
+                  dsn imapsql.db
+                }
+
+                msgpipeline local_routing {
+                  destination $(local_domains) {
+                    # https://maddy.email/tutorials/alias-to-remote/
+                    # vvvvv   This reroute makes it crash    vvvvv
+                    reroute {
+                        #destination $(local_domains) {
+                        #    deliver_to &local_mailboxes
+                        #}
+                        default_destination {
+                          reject 501 5.1.8 "Dummy server"
+                        }
+                    }
+                    # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+                  }
+                  default_destination {
+                    reject 550 5.1.1 "User doesn't exist"
+                  }
+                }
+
+                submission tls://0.0.0.0:465 tcp://0.0.0.0:587 {
+                  auth &send_authdb
+                  source $(local_domains) {
+                    destination $(local_domains) {
+                      deliver_to &local_routing
+                    }
+
+                    default_destination {
+                      reject 501 5.1.8 "Dummy server"
+                    }
+                  }
+                  default_source {
+                    reject 501 5.1.8 "Non-local sender domain"
+                  }
+                }
+
+                imap tls://0.0.0.0:993 tcp://0.0.0.0:143 {
+                  auth &recv_authdb
+                  storage &local_mailboxes
+                }
+
+                # Passwords hashed with `maddyctl hash`
+                auth.pass_table recv_authdb {
+                  table static {
+                    # test
+                    entry postmaster@${domain} bcrypt:$2a$10$tAgW/aaD2KDyMY07MqiFdOfkeBDIZI3.AIHfQTMJDs5t4qdIS6UTW
+                  }
+                }
+              '';
           };
           # Not covered by openFirewall yet
           networking.firewall.allowedTCPPorts = [
